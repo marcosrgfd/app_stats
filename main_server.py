796de5686faadffed123326bbfa0824810889306
marcosrgfd <marcosrgfd2000@gmail.com>
2024-10-31 18:53:22 +0100

    Update main_server.py

--- main_server.py
+++ main_server.py
@@ -426,7 +426,7 @@ def calculate_descriptive_statistics(request_body):
             }
 
             # Crear boxplot por categorías
-            plt.figure(figsize=(12, 9))
+            plt.figure(figsize=(8, 6))
             sns.boxplot(x=category_series, y=data_series1)
             plt.title('Boxplot por Categorías')
             plt.xlabel('Categoría')
